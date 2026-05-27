a44bfbce1ff138274bf07532d61114587390397c
Victor Sandoval <sandoval.nunez.victor.ipn@gmail.com>
2026-05-27 04:32:01 -0600

    Fix SMTP SSL

--- backend/services/mail.service.js
+++ backend/services/mail.service.js
@@ -12,23 +12,17 @@ console.log("EMAIL_PASS existe:", !!process.env.EMAIL_PASS);
 const transporter = nodemailer.createTransport({
 
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
 
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
   },
 
-  family: 4,
-
-  tls: {
-    rejectUnauthorized: false
-  },
-
-  connectionTimeout: 10000,
-  greetingTimeout: 10000,
-  socketTimeout: 10000
+  connectionTimeout: 30000,
+  greetingTimeout: 30000,
+  socketTimeout: 30000
 
 });
 
